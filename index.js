3bec5a2ca02f86fd57b6c7189899621609196474
Aidah Al Baqir <aidah.albaqir@gmail.com>
2021-07-27 00:03:04 -0500

    commit tree 08b13b93e110a5d59df432e9f8ad6469d5704c81

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+function Employee (name, streetAddress) {
+    this.name = name; 
+    this.streetAddress = streetAddress;
+}
+const newEmployee = new Employee ("Sam", "123 Sesame Street");
+
+function updateEmployeeWithKeyAndValue ( employee, key, value){
+     return new Object( newEmployee, "Missy", "456 PickUpStix Way")
+
+}
+
+// const updateEmployeeWithKeyAndValue = new employee (name , streetAddress) {
+//     this.name = name;
+//     this.streetAddress = streetAddress;
+// }
+
+// function updateEmployeeWithKeyAndValue(employee, key, value){
+//     return updateEmployeeWithKeyAndValue (Object(employee), "Bridget", "777 Lucky Way")
+// }
+
+// function destructiveUpdateEmployeeWithKeyandValue(updateEmployeeWithKeyAndValue){
+//     return destructiveUpdateEmployeeWithKeyandValue ("Stephanie", "808 Heartbeats Lane");
+// }
